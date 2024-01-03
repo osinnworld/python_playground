15da8d67d6e66389c9432cab7e3dd3fd77bd8e89
osinnworld <shemogata@gmail.com>
2024-01-03 12:45:22 +0300

    add cheese

--- 01x0_input_output/pizza_pos.py
+++ 01x0_input_output/pizza_pos.py
@@ -8,24 +8,29 @@ if size.lower() not in ['s', 'm', 'l']:
     print("Invalid input, please try again.")
 else:
     if size.lower() == 's':
-        bill += 100
+        bill += 800
         print(f"Small Pizza: KES{bill:,.2f}")
 
     elif size.lower() == 'm':
-        bill += 200
+        bill += 1000
         print(f"Medium Pizza: KES{bill:,.2f}")
 
     elif size.lower() == 'l':
-        bill += 300
+        bill += 1500
         print(f"Large Pizza: KES{bill:,.2f}")
 
     add_pp = input("\nDo you want pepperoni(Y/N)? ")
-
     if add_pp.lower() == 'y':
-        bill += 200
-        print("Add Pepperoni: KES200")
-
+        bill += 300
+        print("Add Pepperoni: KES300")
     elif add_pp.lower() == 'n':
+        pass
+
+    add_ch = input("\nDo you want extra cheese(Y/N)? ")
+    if add_ch.lower() == 'y':
+        bill += 400
+        print("Add Extra Cheese: KES400")
+    elif add_ch.lower() == 'n':
         pass
 
     else:
