f22e487fb3fc894b373b20cf21ed4836453636bd
osinnworld <shemogata@gmail.com>
2024-01-05 13:17:26 +0300

    second way of doing it

--- beginner/tip_calc.py
+++ beginner/tip_calc.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 
-print(":) Welcome to the tip calculator\n")
+print("Welcome to the tip calculator\n")
 
-bill = float(input("Total Bill: $"))
-people = int(input("People to split the bill: "))
+try:
+    bill = float(input("Total Bill: $"))
+    people = int(input("People to split the bill: "))
 
-tip = input("Percentage would you like to give: 10, 12, or 15: ")
+    tip = input("Percentage would you like to give: 10, 12, or 15: ")
 
+    if tip in ['10', '12', '15']:
+        t_bill = bill * (1 + int(tip) / 100)
+        person = t_bill / people
+        print('_' * 50)
+        print(f"Each person should pay: ${person:,.2f}")
+    else:
+        print("Invalid input for tip percentage. Please enter 10, 12, or 15.")
 
-if tip == '10':
-	t_bill = bill*1.10
-elif tip == '12':
-	t_bill = bill*1.12
-elif tip == '15':
-	t_bill = bill*1.15
-else:
-	print("invalid input\n")
+except ValueError:
+    print("Invalid input for bill or number of people. Please enter valid numerical values.")
 
-person = t_bill/people
-print(f"Each person should pay: ${person:,.2f}")	
