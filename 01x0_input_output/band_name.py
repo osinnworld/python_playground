3475b28fea993e82149796ee60ed419979928319
osinnworld <shemogata@gmail.com>
2024-01-05 11:45:22 +0300

    more options

--- 01x0_input_output/band_name.py
+++ 01x0_input_output/band_name.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
+
+import random
+
 print("# Welcome to the band name generator")
 
 b = input("Your City: ")
 c = input("Do you have a pet? (Type Y or N): ")
 
 result = ""
+
 if c.lower() == 'y':
     p = input("Name of Pet: ")
     result = b + p
-    print(f"Your Band Name: {result}")
-elif c.lower() == 'n':
+else:
     l = input("Your Middle Name: ")
     result = b + l
-    print(f"Your Band Name: {result}")
-else:
-    print("Invalid input. Try again")
+
+# Additional customization options
+color = input("Your Favorite Color: ")
+food = input("Your Favorite Food: ")
+hobby = input("Your Hobby: ")
+
+# Incorporate additional customization
+result += color + food + hobby
+
+# Random words
+options = ["Groovy", "Electric", "Funky", "Galactic", "Cosmic", "Revolution", "Echo", "Sonic"]
+random_wd = random.choice(options)
+
+# Mix random words
+result += random_wd
+
+print(f"Your Band Name: {result}")
+
