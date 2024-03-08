ee8137e4886a456e269d32e61695a55e92ad1a51
osinnworld <shemogata@gmail.com>
2024-03-08 07:54:42 +0300

    palindrome with function

--- beginner/palindrome.py
+++ beginner/palindrome.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
+
+def check_palindrome(word):
+    l = len(word)
+    for i in range(l):
+        if word[i] != word[l-1-i]:
+            return False
+    return True
+
 word = input("Enter string: ")
 
-# looping through the letters
-l = len(word)
-palindrome_flag = True
+print('_'*40)
 
-for i in range(l):
-    if word[i] != word[l-1-i]:
-        palindrome_flag = False
-        break
-
-print('_'*30)
-
-if palindrome_flag:
-    print("string is a palindrome")
+if check_palindrome(word):
+    print("String is a palindrome")
 else:
-    print("string is NOT a palindrome")
+    print("String is NOT a palindrome")
+
